294d9c35c113965e952fbfda999f3e6e4f0a0f75
tettehnarh <leslienarh@outlook.com>
2024-05-11 04:00:23 +0000

    Added psycopg2

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculator fi=unctions
+"""
+
+
+def add(x, y):
+    """Add x and y and return the results"""
+    return x + y
+
+
+def subtract(x, y):
+    """Subtract y from x and return the result"""
+    return x - y
